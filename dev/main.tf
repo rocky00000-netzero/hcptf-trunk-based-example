80cfc9c9114fad9c651853cbc5ab630a0376b8e6
rocky00000-netzero <130683351+rocky00000-netzero@users.noreply.github.com>
2025-04-17 17:14:25 +0900

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -24,3 +24,9 @@ resource "random_pet" "example_three" {
   prefix    = var.pet_prefix
   separator = var.pet_separator
 }
+
+resource "random_pet" "example_four" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
